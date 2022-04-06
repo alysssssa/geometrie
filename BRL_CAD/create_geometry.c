1c49f47507e9a714f21ef7fa2e693b7c97c2d2a8
alyssa liu <alyssa.h.liu@gmail.com>
2022-04-06 16:14:35 +0100

    can do boolean operations now

--- BRL_CAD/create_geometry.c
+++ BRL_CAD/create_geometry.c
@@ -6,7 +6,7 @@
 
 int main(int argc, const char * argv[]){
     struct rt_wdb *wdbp;
-    char new_file_name[50] = "geom.g";
+    char new_file_name[50] = "output.g";
     if((wdbp=wdb_fopen(new_file_name))==RT_WDB_NULL){
          bu_log("Failed to open output file (%s)\n", new_file_name);
         exit(1);
@@ -34,6 +34,31 @@ int main(int argc, const char * argv[]){
     /* build the sphere */
     if(mk_sph(wdbp, sphere_name, center, radius)){
         bu_log("Failed to make a sphere (%s)\n", sphere_name);
+        exit(1);
+    }
+    
+    /* build a combination */
+    struct bu_list head;
+    struct wmember *wmp;
+    
+    /* initialize the list of members for this combination */
+    BU_LIST_INIT(&head);
+    
+    /* add the first member - sphere - (use the UNION operator here) */
+    if((wmp=mk_addmember("s1", &head, NULL, WMOP_UNION)) == WMEMBER_NULL){
+        bu_log("Failed to add member(s1)\n");
+        exit(1);
+    }
+    
+    /* subtract the second member - box */
+    if((wmp=mk_addmember("b1", &head, NULL, WMOP_SUBTRACT)) == WMEMBER_NULL){
+        bu_log("Failed to add member(b1)\n");
+        exit(1);
+    }
+    
+    /* build the region */
+    if( mk_comb(wdbp, "region1", &head, 1, NULL, NULL, NULL, 1000, 0, 1, 100, 0, 0, 0)){
+        bu_log("Error building region\n");
         exit(1);
     }
     
